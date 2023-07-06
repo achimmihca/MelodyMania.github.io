7900b8dccfe95e83d3f0c8cb3a43a3dd91942c90
Andreas Stange <anst.dev@gmail.com>
2023-07-06 20:08:11 +0200

    steam download button instead of wishlist button

--- components/DownloadCallToActionSection.tsx
+++ components/DownloadCallToActionSection.tsx
@@ -9,7 +9,7 @@ const DownloadCallToActionSection = () =>
             <h1 className="text-center font-alt mb-4"><T i18nKey="downloadCallToActionSection_title" /></h1>
             {/* Buttons  */}
             <div className="d-flex flex-column flex-lg-row align-items-center">
-                <a className="btn btn-lg mb-2" href="https://store.steampowered.com/app/2394070/Melody_Mania/" rel="noreferrer" style={{position: 'relative', width: "300px", height: '80px', marginLeft: "5px", marginRight: "5px"}}><Image src="img/buttons/Steam-WishlistButton.svg" layout="fill" objectFit="contain"/></a>
+                <a className="btn btn-lg mb-2" href="https://store.steampowered.com/app/2394070/Melody_Mania/" rel="noreferrer" style={{position: 'relative', width: "300px", height: '80px', marginLeft: "5px", marginRight: "5px"}}><Image src="img/buttons/Steam-DownloadButton.svg" layout="fill" objectFit="contain"/></a>
                 <a className="btn btn-lg mb-2" href="https://play.google.com/store/apps/details?id=com.melodymania.MelodyManiaCompanion" target="_blank" rel="noreferrer" style={{position: 'relative', width: "300px", height: '80px', marginLeft: "5px", marginRight: "5px"}}><Image src="img/buttons/Google_Play_Store_badge_companion_app.svg" layout="fill" objectFit="contain"/></a>
             </div>
         </div>
