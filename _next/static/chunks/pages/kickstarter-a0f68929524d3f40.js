9700aeee21411078b92e7bd3bb232e1894b355af
achimmihca <anst.dev@gmail.com>
2024-06-22 14:22:49 +0000

    Deploying to gh-pages from @ achimmihca/MelodyMania.github.io@7d993b39da0fcf7edefccc57aa598a8222ff2d2e 🚀

---
+++ _next/static/chunks/pages/kickstarter-a0f68929524d3f40.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[314],{3879:function(n,t,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/kickstarter",function(){return u(1308)}])},1308:function(n,t,u){"use strict";u.r(t);var _=u(7137);t.default=(0,_.pm)("https://melodymania.org")}},function(n){n.O(0,[137,888,774,179],function(){return n(n.s=3879)}),_N_E=n.O()}]);
