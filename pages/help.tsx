e6195d71cad6539cbc4dd5eb4e3dea8d832e3a81
Andreas Stange <anst.dev@gmail.com>
2025-12-13 20:44:43 +0100

    help: show warning on for firefox desktop users

--- pages/help.tsx
+++ pages/help.tsx
@@ -1,14 +1,37 @@
 import { NextPage } from "next"
 import BackToHomeNavigationSection from "../components/BackToHomeNavigationSection";
 import { useTranslationUnescaped } from "../lib/utils";
+import { useEffect, useState } from "react";
+import Bowser from "bowser";
 
 const Help: NextPage = () =>
 {
     const { t } = useTranslationUnescaped()
+    const [isFirefoxMobile, setIsFirefoxMobile] = useState(false);
+
+    useEffect(() => {
+        console.log(navigator);
+        console.log(Bowser.getParser(window.navigator.userAgent).getBrowserName());
+        if (typeof navigator !== "undefined") {
+            const browser = Bowser.getParser(window.navigator.userAgent);
+            const isFirefox = browser.getBrowserName() === "Firefox";
+            const isMobile = browser.getPlatformType(true) === "mobile";
+            setIsFirefoxMobile(isFirefox && !isMobile);
+        }
+    }, []);
 
     return (
         <>
             <BackToHomeNavigationSection title={`Melody Mania - Help`} />
+
+            {/* The AI Chat does not work on Firefox because of security issues in the iframe. */}
+            {isFirefoxMobile && (
+                <div style={{display: "flex", position: "absolute", justifyContent: "center", alignItems: "center", height: "100%", width: "100%"}}>
+                    <div style={{ color: "red", fontWeight: "bold", fontSize: "1.2rem", textAlign: "center" }}>
+                        The AI chat is currently not supported in Firefox. Please use Chrome or Edge instead.
+                    </div>
+                </div>
+            )}
 
             <iframe
                 src="https://app.thinkstack.ai/bot/index.html?chatbot_id=693307a354cc835c79daae79&type=inline"
