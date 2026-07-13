dfedac5750421ef5b30144696cbaea8c9766bcf7
Andreas Stange <anst.dev@gmail.com>
2026-07-13 08:05:23 +0200

    add utm_source parameter to steam store link

--- pages/get.tsx
+++ pages/get.tsx
@@ -1,2 +1,2 @@
 import { getCustomRedirect } from '../lib/redirect'
-export default getCustomRedirect('https://store.steampowered.com/app/2394070/Melody_Mania/')
+export default getCustomRedirect('https://store.steampowered.com/app/2394070/Melody_Mania?utm_source=homepage_redirect')
