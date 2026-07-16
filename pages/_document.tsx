839e0c6dcf157daf1b7c8b39128cc8caa9a9d285
Andreas Stange <anst.dev@gmail.com>
2026-07-16 11:47:04 +0200

    Revert "remove Google Tag manager"
    This reverts commit 7ed8a46baf009ae3708db8dbf29774a457ecb677.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -20,6 +20,14 @@ class MyDocument extends Document
           ` }} />
           {/* End Google Tag */}
           
+          {/* Google Tag Manager */}
+          <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TJM7MVSH');` }} />
+          {/* End Google Tag Manager */}
+
           {/* Event snippet for 'Click on Steam Store Link' conversion page */}
           <script dangerouslySetInnerHTML={{ __html: `function gtag_report_conversion(url, evt) {
   evt.preventDefault();
